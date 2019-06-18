4e2bf7c3410dcb5519287505e5d337f20d9c231c
Mark Aziz <m.aziz@crimsoneducation.org>
2019-06-18 20:53:05 +1200

    setup pm2 deployment;

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'spotify-backend',
+    script: './index.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-13-239-6-208.ap-southeast-2.compute.amazonaws.com',
+      key: '~/.ssh/spotify-app-node.pem',
+      ref: 'origin/master',
+      repo: 'git@github.com:m-aziz/spotify-backend.git',
+      path: '/home/ubuntu/spotify-backend',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
